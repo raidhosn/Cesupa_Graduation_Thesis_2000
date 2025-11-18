8fb0fd9473b1990774e76cd99fa992a74fe1c305
Raid Aboul Hosn <78128906+raidhosn@users.noreply.github.com>
2025-11-18 11:56:11 -0800

    Added package.json

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+export default {
+  root: '.',
+  publicDir: false,
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+};
